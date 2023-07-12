11800c68e15dd26152dcef07e39e5a146f42f5b3
Yergeni Cabranes <yergenis@gmail.com>
2023-07-11 20:56:50 -0400

    feat: configure next auth and prisma client

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,82 @@
+import NextAuth, {AuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+// import { PrismaClient } from "@prisma/client";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import prisma from "@/app/libs/prismadb";
+// const prisma = new PrismaClient();
+
+import bcrypt from "bcrypt";
+
+export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID as string,
+			clientSecret: process.env.GITHUB_SECRET as string,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+		CredentialsProvider({
+			// The name to display on the sign in form (e.g. 'Sign in with...')
+			name: "credentials",
+			// The credentials is used to generate a suitable form on the sign in page.
+			// You can specify whatever fields you are expecting to be submitted.
+			// e.g. domain, username, password, 2FA token, etc.
+			// You can pass any HTML attribute to the <input> tag through the object.
+			credentials: {
+				email: { label: "email", type: "text" },
+				password: { label: "password", type: "password" },
+			},
+			async authorize(credentials, req) {
+				// You need to provide your own logic here that takes the credentials
+				// submitted and returns either a object representing a user or value
+				// that is false/null if the credentials are invalid.
+				// e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+				// You can also use the `req` object to obtain additional parameters
+				// (i.e., the request IP address)
+				if (!credentials?.email || !credentials?.password) {
+					throw new Error("Invalid credentials");
+				}
+
+				const user = await prisma.user.findUnique({
+					where: {
+						email: credentials.email,
+					},
+				});
+
+				// If user does not exist or bad password
+				if (!user || !user.hashedPassword) {
+					throw new Error("Invalid credentials");
+				}
+
+				// Check input password vs user password in DB
+				const isCorrectPassword = await bcrypt.compare(
+					credentials.password,
+					user.hashedPassword
+				);
+
+				if (!isCorrectPassword) {
+					throw new Error("Invalid credentials");
+				}
+
+				return user;
+			},
+		}),
+	],
+	pages: {
+		signIn: '/', // home route since it is render in a modal
+	},
+	debug: process.env.NODE_ENV === "development",
+	session: {
+		strategy: 'jwt'
+	},
+	secret: process.env.NEXTAUTH_SECRET
+};
+
+export default NextAuth(authOptions);
