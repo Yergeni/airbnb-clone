0d8296b058d5b37ad6014836239e5a462e2afa75
Yergeni Cabranes <yergenis@gmail.com>
2023-07-21 11:29:01 -0400

    feat: add category step and logic

---
+++ src/app/components/inputs/CategoryInput.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+type CategoryInputProps = {
+	icon: IconType;
+	label: string;
+	onClick: (value: string) => void;
+	selected?: boolean;
+};
+
+export default function CategoryInput({
+	icon: Icon,
+	label,
+	selected,
+	onClick,
+}: CategoryInputProps) {
+	return (
+		<button
+			onClick={() => onClick(label)}
+			className={`w-full rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer ${
+				selected ? "border-black" : "border-neutral-200"
+			}`}
+		>
+			<Icon size={30} />
+			<p className="font-semibold">{label}</p>
+		</button>
+	);
+}
---
+++ src/app/components/modals/types.ts
@@ -0,0 +1,11 @@
+export type RentFormValue = {
+	category: string;
+	location: any;
+	guestCount: number;
+	roomCount: number;
+	bathroomCount: number;
+	imageSrc: string;
+	price: number;
+	title: string;
+	description: string;
+};
