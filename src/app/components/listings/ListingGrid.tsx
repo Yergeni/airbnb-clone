c98d3a7d6a3871543dc4f8159ddc475111ea570c
Yergeni Cabranes <yergenis@gmail.com>
2023-08-15 14:42:18 -0400

    feat: add a resusable Listing Grid layout

---
+++ src/app/components/listings/ListingGrid.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ReactNode } from "react";
+
+type ListingGridProps = {
+	sectionId: string;
+	children: ReactNode;
+	isHomePage?: boolean
+};
+
+export default function ListingGrid({ sectionId, children, isHomePage }: ListingGridProps) {
+	return (
+		<section
+			id={sectionId}
+			className={`
+				${isHomePage ? "pt-24" : "mt-10"}
+				grid grid-cols-1 
+				sm:grid-cols-2 
+				md:grid-cols-3 
+				lg:grid-cols-4 
+				xl:grid-cols-5 
+				2xl:grid-cols-6 
+				gap-8
+			`}
+		>
+			{children}
+		</section>
+	);
+}
